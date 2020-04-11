6900db0def58778186dc11f4c77e937ac249bc75
Alphadrake <57242694+Alphadrake86@users.noreply.github.com>
2020-04-10 20:20:32 -0700

    this was a nightmare
    I'm terrible at this

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ var $ = function(id){
     return document.getElementById(id)
 }
 
+
 var generateIdentity = function(){
-var identity = [];
+    var identity = [];
     rand(1,2)==1?
         identity.push("Tom"):
         identity.push("She-cat")
@@ -37,9 +38,9 @@ var identity = [];
         else if (age<=17) rank = "Senior warrior"
         else rank = "Elder"
 
-        if(age<13 && age>6 && identity[0]=="She-cat" && i%10 < 5) rank = "Queen"
+        if(age<16 && age>6 && identity[0]=="She-cat" && i%10 < 5) rank = "Queen"
 
-        if(age>10 && age<17 && rand(1,3)<2){
+        if(age>9 && age<17 && rand(1,3)<2){
             if(i%10<=3) {rank = "Medicine cat"}
             else if(i%10<=6) {rank = "Deputy"}
             else {rank = "Leader"}
@@ -55,29 +56,35 @@ var displayIdentity = function(identity){
     $("age").innerHTML =    "     Age/Rank: " + identity[2]
 }
 
-var generateStats = function(){
-    lvldata = ["Terrible", "Terrible+",
-              "Bad", "Bad+",
-              "Poor","Poor+","Poor+",
-              "Average","Average","Average+","Average+",
-              "Good","Good","Good+",
-               "Great","Great+",
-              "Excellent","Excellent+"]
+
+var generateStats = function(words){
     var avg = 0
-    var len = lvldata.length-1
-    var stats = [rand(0,len),rand(0,len),rand(0,len),rand(0,len),rand(0,len),rand(0,len)]
-    stats.forEach(function(item) {
+    var len = 24 // length of lvldata below
+    var statdata = [rand(0,len),rand(0,len),rand(0,len),rand(0,len),rand(0,len),rand(0,len)]
+    statdata.forEach(function(item) {
         avg+=item
     });
-    stats.push(Math.floor(avg/6))
-    var statdata = []
+    statdata.push(Math.floor(avg/6))
+    
+    return [stats2words(statdata,words),statdata]
+}
 
-    stats.forEach(function(item, index, arr) {
-        statdata[index] = item
-        arr[index] = lvldata[item]
+    
+var stats2words = function(statdata, words){
+    words = words%3
+    lvldata = [["Terrible","F",1,], ["Terrible","F+",2,],
+    ["Bad","E",3,], ["Bad","E+",4,],["Bad","E+",5,],
+    ["Poor","D",6,],["Poor","D",7,],["Poor","D+",8,],["Poor","D+",9,],
+    ["Average","C",10,],["Average","C",11,],["Average","C",12,],["Average","C+",13,],["Average","C+",14,],["Average","C+",15,],
+    ["Good","B",16,],["Good","B",17,],["Good","B+",18,],["Good","B+",19,],
+    ["Great","A",20,],["Great","A",21,],["Great","A+",22],
+    ["Excellent","S",23,],["Excellent","S+",24]]
+    var stats = []
+    statdata.forEach(function(value) {
+        stats.push(lvldata[value][words])
     });
-    console.log(statdata)
-    return [stats,statdata]
+    
+    return stats
 }
 
 var displayStats = function(stats){
@@ -88,124 +95,6 @@ var displayStats = function(stats){
     $("def").innerHTML= "Defense " + stats[0][4]
     $("tac").innerHTML= "Tactics:  " + stats[0][5]
     $("avg").innerHTML= "Total: " + stats[0][6]
-}
-
-var generateFeatures = function(){
-    var features = []
-    var builds = ["Thin, lithe, light",
-                "Short, stocky, broad",
-                "Tall, fit, lanky",
-                "Thick, muscled, heavy"]
-    features.push(builds[rand(0,builds.length-1)])
-
-    var eyecolordata = ["Navy",
-                    "Blue",
-                    "Ice",
-                    "Aquamarine",
-                    "Green",
-                    "Chartreuse",
-                    "Yellow",
-                    "Orange",
-                    "Amber",
-                    "Caramel",
-                    "Russet",
-                    "Silver"]
-    
-    var eyecolors = [eyecolordata[rand(0,eyecolordata.length-1)]]
-
-    var dbts = 0
-    var i = rand(1,20)
-    if(i>12) {dbts = 1}
-    if(i>17) {dbts = 2}
-
-    var disabilities = []
-    while(disabilities.length<dbts){
-        var db = weightedDisability()
-        if(!disabilities.includes(db)) disabilities.push(db)
-    }
-
-    disabilities.forEach(function(val, index, arr){
-        if(val == "eyes"){
-            var i = rand(1,20)
-            if(i<= 8){
-                arr[index] = "Heterochromia"
-                eyecolors.push(eyecolors[0])
-                var nc = eyecolordata[rand(0,eyecolordata.length-1)]
-                while(eyecolors.includes(nc)) nc = eyecolordata[rand(0,eyecolordata.length-1)]
-                eyecolors[rand(0,1)] = nc
-            }
-            else if(i<=16){
-                eyecolors.push(eyecolors[0])
-                var eye = rand(0,1)
-                var type = rand(0,1)
-                arr[index] = type==0 ? "Half Blind" : "Missing Eye"
-                eyecolors[eye] = type==0 ? "Cloudy " : "Missing"
-            } 
-            else {
-                if(rand(1,3)<2)eyecolors[0] = "Cloudy"
-                arr[index] = "Blind"
-            }
-        }
-        else if(val == "deaf"){
-            var i = rand(1,10)
-            arr[index] = i<8 ? "Half Deaf" : "Deaf"
-        }
-    })
-    if(disabilities.includes("Blind") && disabilities.includes("Facial Scars")){
-        eyecolors[0] = "Missing"
-    }
-    features.push(eyecolors)
-    features.push(disabilities)
-    features.push(generateFur())
-    return features
-}
-
-var displayFeatures = function(features){
-    $("fur").innerHTML = "Fur: " + features[3]
-    $("body").innerHTML = "Body type: " + features[0]
-    if(features[1].length == 1){
-        $("eyes").innerHTML = "Eye color: " + features[1][0]
-    }
-    else{
-        $("eyes").innerHTML = "Left eye: " + features[1][0] + "<br>Right eye: " + features[1][1]
-    }
-
-    if(features[2].length == 0){
-        $("scars").innerHTML = "Scars/Disabilities: None"
-    }
-    else if(features[2].length == 1){
-        $("scars").innerHTML = "Scars/Disabilities: " + features[2][0]
-    }
-    else{
-        $("scars").innerHTML = "Scars/Disabilities: " + features[2][0] + " and " + features[2][1]
-    }
-}
-var generateFur = function(){
-    var fur = []
-    lengths = ["Short",
-            "Medium",
-            "Long",
-            "Thick",
-            "Scruffy",
-            "Wiry",
-            "Coarse"]
-    fur.push(lengths[rand(1,lengths.length)-1])
-    colors = [
-        "black",
-        "grey",
-        "silver",
-        "white",
-        "ginger",
-        "orange",
-        "gold",
-        "cream",
-        "coffee",
-        "brown",
-        "tan",
-        "sienna"
-    ]
-    fur.push(colors[rand(1,colors.length)-1])
-    return fur[0] + " " + fur[1] + " fur"
 }
 
 var generatePersonalities = function(){
@@ -279,7 +168,126 @@ var displayPersonalities = function(personality){
     }
     $("pos").innerHTML =  stringgen()
 
+}
 
+var generateFeatures = function(){
+    var features = []
+    var builds = ["Thin, lithe, light",
+                "Short, stocky, broad",
+                "Tall, fit, lanky",
+                "Thick, muscled, heavy"]
+    features.push(builds[rand(0,builds.length-1)])
+
+    var eyecolordata = ["Navy",
+                    "Blue",
+                    "Ice",
+                    "Aquamarine",
+                    "Green",
+                    "Chartreuse",
+                    "Yellow",
+                    "Orange",
+                    "Amber",
+                    "Caramel",
+                    "Russet",
+                    "Silver"]
+    
+    var eyecolors = [eyecolordata[rand(0,eyecolordata.length-1)]]
+
+    var dbts = 0
+    var i = rand(1,20)
+    if(i>12) {
+        dbts = 1
+    }
+    if(i>18) {
+        dbts = 2
+    }
+
+    var disabilities = []
+    while(disabilities.length<dbts){
+        var db = weightedDisability()
+        if(!disabilities.includes(db)) disabilities.push(db)
+    }
+
+    disabilities.forEach(function(val, index, arr){
+        if(val == "eyes"){
+            var i = rand(1,20)
+            if(i<= 8){
+                arr[index] = "Heterochromia"
+                eyecolors.push(eyecolors[0])
+                var nc = eyecolordata[rand(0,eyecolordata.length-1)]
+                while(eyecolors.includes(nc)) nc = eyecolordata[rand(0,eyecolordata.length-1)]
+                eyecolors[rand(0,1)] = nc
+            }
+            else if(i<=16){
+                eyecolors.push(eyecolors[0])
+                var eye = rand(0,1)
+                var type = rand(0,1)
+                arr[index] = type==0 ? "Half Blind" : "Missing Eye"
+                eyecolors[eye] = type==0 ? "Milky" : "Missing"
+            } 
+            else {
+                if(rand(1,3)<2)eyecolors[0] = "Milky"
+                arr[index] = "Blind"
+            }
+        }
+        else if(val == "deaf"){
+            var i = rand(1,10)
+            arr[index] = i<8 ? "Half Deaf" : "Deaf"
+        }
+    })
+    features.push(eyecolors)
+    features.push(disabilities)
+    features.push(generateFur())
+    return features
+}
+
+var displayFeatures = function(features){
+    $("fur").innerHTML = "Fur: " + features[3]
+    $("body").innerHTML = "Body type: " + features[0]
+    if(features[1].length == 1){
+        $("eyes").innerHTML = "Eye color: " + features[1][0]
+    }
+    else{
+        $("eyes").innerHTML = "Left eye: " + features[1][0] + "<br>Right eye: " + features[1][1]
+    }
+
+    if(features[2].length == 0){
+        $("scars").innerHTML = "Scars: None"
+    }
+    else if(features[2].length == 1){
+        $("scars").innerHTML = "Scars: " + features[2][0]
+    }
+    else{
+        $("scars").innerHTML = "Scars: " + features[2][0] + " and " + features[2][1]
+    }
+}
+
+var generateFur = function(){
+    var fur = []
+    lengths = ["Short",
+            "Medium",
+            "Long",
+            "Thick",
+            "Scruffy",
+            "Wiry",
+            "Coarse"]
+    fur.push(lengths[rand(1,lengths.length)-1])
+    colors = [
+        "black",
+        "grey",
+        "silver",
+        "white",
+        "ginger",
+        "orange",
+        "gold",
+        "cream",
+        "coffee",
+        "brown",
+        "tan",
+        "sienna"
+    ]
+    fur.push(colors[rand(1,colors.length)-1])
+    return fur[0] + " " + fur[1] + " fur"
 }
 
 var weightedDisability = function(){
@@ -291,9 +299,33 @@ var weightedDisability = function(){
     if(i<70) return "Tailless"
     if(i<77) return "deaf"
     if(i<80) return "Mute"
-    if(i<84) return "Lame leg"
-    if(i<85) return "Missing leg"
+    if(i<85) return "Lame leg"
+    if(i<86) return "Missing leg"
     if(i<87) return "Hairless"
     if(i<92) return "Twisted paw"
     return "Unlucky"
+}
+
+var Cat = {
+    identity:[],
+    stats:[],
+    features:[],
+    personalities:[],
+    words:0,
+
+    reiden:function(){identity = generateIdentity()},
+    restat:function(){stats = generateStats(this.words)},
+    refeat:function(){features=generateFeatures()},
+    repers:function(){personalities=generatePersonalities()},
+    refresh:function(){
+                    displayIdentity(identity)
+                    displayStats(stats)
+                    displayFeatures(features)
+                    displayPersonalities(personalities)
+                },
+    swapstats:function(){
+        
+        stats[0] = stats2words(stats[1],++this.words)
+        }
+
 }
